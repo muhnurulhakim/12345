209e5c5686205b3b2e1395e1d0fae712326552a7
muhnurulhakim <116804209+muhnurulhakim@users.noreply.github.com>
2024-05-16 22:31:31 +0700

    Add files via upload

---
+++ _posts/2024-01-01-welcome-to-my-blog.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Welcome to My Blog"
+date: 2024-01-01
+---
+
+This is the first post on my new blog!
+
---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+(function($) {
+    // Navigation
+    var $nav = $('#nav');
+    if ($nav.length > 0) {
+        $nav.children('ul').addClass('links');
+    }
+})(jQuery);
+
